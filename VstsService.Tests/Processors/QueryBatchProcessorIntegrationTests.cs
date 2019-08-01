078236a78986b979e433edd70d80369d7565a848
Adriaan Thomas <Adriaan.Thomas@rabobank.nl>
2019-08-01 12:39:27 +0000

    Merged PR 27251: Add processor for WIQL queries in page sizes, to handle > 20k work items
    Add processor for WIQL queries in page sizes, to handle > 20k work items
    
    Related work items: #818574

---
+++ VstsService.Tests/Processors/QueryBatchProcessorIntegrationTests.cs
@@ -0,0 +1,41 @@
+using SecurePipelineScan.VstsService.Processors;
+using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace SecurePipelineScan.VstsService.Tests.Processors
+{
+    /// <summary>
+    /// Tests work item retrieval in batches.
+    /// </summary>
+    /// <remarks>
+    /// As we don't have enough work items in our default organisation (raboweb-test), we run this on raboweb with the
+    /// project hard coded. When running this test, make sure your token in appsettings.user.json or an environment
+    /// variable is valid for raboweb.
+    /// </remarks>
+    /// <see cref="TestConfig"/>
+    public class QueryBatchProcessorIntegrationTests : IClassFixture<TestConfig>
+    {
+        private const string Project = "Investments";
+
+        private readonly IQueryBatchProcessor _processor;
+
+        public QueryBatchProcessorIntegrationTests(TestConfig config)
+        {
+            var client = new VstsRestClient("raboweb", config.Token);
+            _processor = new QueryBatchProcessor(client);
+        }
+
+        [Fact(Skip = "integration")]
+        public void ShouldBeAbleToReceiveMoreThan20KWorkItems()
+        {
+            // Arrange
+
+            // Act
+            var resultCount = _processor.QueryByWiql(Project).Count();
+
+            // Assert
+            resultCount.ShouldBeGreaterThan(20_000);
+        }
+    }
+}
